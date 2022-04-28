738481f7c0930fbff4860f7be37df40c1f527465
Yashwin <yashwinpoojary@gmail.com>
2022-04-28 12:11:33 +0530

    Implement site table

---
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/index.jsx
@@ -0,0 +1,10 @@
+import SiteTable from '../site-table';
+
+export default function SitesOverview() {
+	return (
+		<div className="sites-overview">
+			<div>My Sites</div>
+			<SiteTable />
+		</div>
+	);
+}
