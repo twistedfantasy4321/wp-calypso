21be50a7f1ab902f61652abbf82114dcb5cc3f0c
Chris Diehl <jjchrisdiehl@gmail.com>
2022-05-03 16:31:31 -0400

    Add display price to endpoint and create setter function

--- client/my-sites/plan-price/index.jsx
+++ client/my-sites/plan-price/index.jsx
@@ -19,6 +19,7 @@ export class PlanPrice extends Component {
 			displayPerMonthNotation,
 			displayShortPerMonthNotation,
 			originalPricePrefix,
+			productDisplayPrice,
 			isOnSale,
 			taxText,
 			translate,
@@ -26,6 +27,11 @@ export class PlanPrice extends Component {
 
 		if ( ! currencyCode || ! ( typeof rawPrice === 'number' ) ) {
 			return null;
+		}
+		// Dangerously set innerHTML display price
+		// eslint-disable-next-line no-unused-vars
+		function setDisplayPrice() {
+			return { __html: productDisplayPrice };
 		}
 
 		// "Normalize" the input price or price range.
